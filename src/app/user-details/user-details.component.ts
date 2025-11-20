3731e9a6dc7fdf80cdbd27bbaca16018b7c56841
dscarrig <dcarrig1@gmail.com>
2025-11-20 12:37:06 -0600

    Update to angular 19

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-user-details',
-  templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.css']
+    selector: 'app-user-details',
+    templateUrl: './user-details.component.html',
+    styleUrls: ['./user-details.component.css'],
+    standalone: false
 })
 export class UserDetailsComponent {
 
